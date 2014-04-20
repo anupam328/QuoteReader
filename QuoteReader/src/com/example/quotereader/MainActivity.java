b78912b7cf42a5263e604052a3c217547e8625c4
anupam <anupam328@gmail.com>
2014-04-19 23:46:54 -0700

    YoSanjanaSoSmart

--- QuoteReader/src/com/example/quotereader/MainActivity.java
+++ QuoteReader/src/com/example/quotereader/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.quotereader;
-
+//YoSanjanaSoSmart
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
